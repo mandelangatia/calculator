a242fb0ce59900489babba7e36134dda3be551d7
caroltheuri <carolinetheuriwangari2@gmail.com>
2017-05-30 14:43:32 +0300

    created calculator

---
+++ js/script.js
@@ -0,0 +1,36 @@
+var addition=function(num1,num2)
+{
+  var num1=parseInt(prompt("Enter first number"));
+  var num2=parseInt(prompt("Enter second number"));
+  result= num1+num2;
+  alert("the sum is "+ result);
+};
+  addition();
+var subtraction=function(num1,num2){
+  var num1=parseInt(prompt("Enter first number"));
+  var num2=parseInt(prompt("Enter second number"));
+  result2= num1-num2;
+  alert("the diff is "+ result2);
+  };
+  subtraction();
+var multiplication=function(num1,num2){
+var num1=parseInt(prompt("Enter first number"));
+var num2=parseInt(prompt("Enter second number"));
+result3=num1*num2;
+alert("the multiplication is "+ result3);
+};
+multiplication();
+var division=function(num1,num2){
+  var num1=parseInt(prompt("Enter first number"));
+  var num2=parseInt(prompt("Enter second number"));
+  result4=num1/num2;
+  alert("the division is "+ result4);
+};
+division();
+var modulo=function(num1,num2){
+  var num1=parseInt(prompt("Enter first number"));
+  var num2=parseInt(prompt("Enter second number"));
+  result5=num1%num2;
+  alert("the modulo is "+ result5);
+};
+  modulo();
